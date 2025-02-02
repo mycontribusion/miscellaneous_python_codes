f047f568f2f63808040a3ad019b84264070f78ea
mycontribusion <127086525+mycontribusion@users.noreply.github.com>
2025-02-02 15:38:33 +0100

    Add files via upload
    This python code automates the process of calculating image dimensions. How?
    
    1) receives two inputs: width and height ( from the .txt file you create)
    2) calculates their ratio.
    3) starts a while loop
    3) receives desired width
    4) outputs desired height
    
    Example of usage:
    
    First scenario:
    
       - lets say the dimension of an image is 130px x 70px
       - but would want an image with same ratio but with width of 10000px
    
       this code would easily calculate the height
    
    Second scenario:
    
       - canva users may not be alllowed to design
       - an image with width of upto 10000px with the free version
       - with this code one can repeatedly try several width sizes
       - while maintaing the ratio.

---
+++ dimension1.py
@@ -0,0 +1,28 @@
+with open("dimension", "r") as file:
+    dimensions = []
+    for line in file:
+        words = line.split()
+    
+        for word in words:
+            dimensions.append(word)
+
+width = dimensions[0]
+height = dimensions[1]
+
+ratio = int(width)/int(height)
+
+print(f"Good! the width ratio height is {ratio}")
+
+while True:
+
+    desired_width = int(input("enter desired width: "))
+
+    desired_height = desired_width/ratio
+
+    output_text = f'dimension = {desired_width}x{int(desired_height)}'
+
+    lenght_of_text = len(output_text)
+    underline = '_'*lenght_of_text
+
+    print(f'\n{underline}')
+    print(f'{output_text}\n')
